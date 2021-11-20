d19944f4dedc5b473e379b0d7f8af910026f253d
kdjay517 <kdjay517@gmail.com>
2021-11-21 02:14:58 +0530

    Added: Removing a word from a sentence

--- src/com/bridglabz/hashtable/Operations.java
+++ src/com/bridglabz/hashtable/Operations.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Operations {
 	public void frequencyOfWords() {
-		System.out.println("Enter a sentance to find frequency of words:");
+		System.out.println("Write a sentance to find frequency of words:");
 		Scanner sc = new Scanner(System.in);
 		String sentance = sc.nextLine();
 		String[] words = sentance.split(" ");
@@ -22,7 +22,7 @@ public class Operations {
 	}
 	
 	public void frequencyOfWordsInParagraph() {
-		System.out.println("Enter a paragraph to find frequency of words:");
+		System.out.println("Write a paragraph to find frequency of words:");
 		Scanner sc = new Scanner(System.in);
 		String sentance = sc.nextLine();
 		String[] words = sentance.split(" ");
@@ -36,8 +36,26 @@ public class Operations {
 				value += 1;
 			hashMap.put(word, value);	
 		}
+		System.out.println(hashMap);	
+	}
+	
+	public void removingWord() {
+		System.out.println("Write a paragraph to word:");
+		Scanner sc = new Scanner(System.in);
+		String sentance = sc.nextLine();
+		String[] words = sentance.split(" ");
+		LinkedHashMap<String, Integer> hashMap = new LinkedHashMap<>();
+		for (int i = 0;i < words.length;i++) {
+			String word = words[i];
+			Integer value = hashMap.get(word);
+			if (value == null) {
+				value = 1;
+			}else
+				value += 1;
+			hashMap.put(word, value);
+			hashMap.remove("not");;	
+		}
 		System.out.println(hashMap);
-		
 	}
 
 }
--- src/com/bridglabz/hashtable/Runner.java
+++ src/com/bridglabz/hashtable/Runner.java
@@ -6,7 +6,7 @@ public class Runner {
 	public static void main(String[] args) {
 		Operations operate = new Operations();
 		System.out.println("\nEnter 1 to find frequency of words in a sentance\nEnter 2 to find frequency"
-				+ "of words in a paragraph");
+				+ "of words in a paragraph\nEnter 3 to delete a word from paragraph");
 		Scanner sc = new Scanner(System.in);
 		switch (sc.nextInt()) {
 		case 1:
@@ -14,6 +14,9 @@ public class Runner {
 			break;
 		case 2:
 			operate.frequencyOfWordsInParagraph();
+			break;
+		case 3:
+			operate.removingWord();
 			break;
 		default:
 			sc.close();
